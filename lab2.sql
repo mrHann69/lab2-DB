ef96c32562af9fcd251d37cc39d107c59814d164
mrHann69 <matheo.franco@correounivalle.edu.co>
2021-03-22 18:12:29 -0500

    -nada

--- lab2.sql
+++ lab2.sql
@@ -1,3 +1,5 @@
+IF EXIST
+
 --PUNTO 1
 CREATE TABLE X (
 	B varchar(2),
@@ -11,25 +13,22 @@ CREATE TABLE Z (
 	A varchar(2),
 	C varchar(2)
 );
-INSERT INTO X (B, C) VALUES (
-	(b1, c1),
-  (b2, c2),
-  (b2, c1),
-  (b1, c3),
-  (b1, c5)
-);
-INSERT INTO Y (A, B) VALUES (
-	(a1, b1),
-  (a1, b3),
-  (a1, b4),
-  (a3, b1)
-);
-INSERT INTO Z (A, C) VALUES (
-	(a1, c2),
-  (a2, c2),
-  (a2, c1),
-  (a3, b3),
-);
+INSERT INTO X VALUES
+	('b1', 'c1'),
+	('b2', 'c2'),
+  	('b2', 'c1'),
+  	('b1', 'c3'),
+  	('b1', 'c5');
+INSERT INTO Y VALUES
+	('a1', 'b1'),
+  	('a1', 'b3'),
+  	('a1', 'b4'),
+  	('a3', 'b1');
+INSERT INTO Z VALUES
+	('a1', 'c2'),
+  	('a2', 'c2'),
+  	('a2', 'c1'),
+  	('a3', 'b3');
 
 --CONSULTAS
 --X interseccion Y
@@ -37,7 +36,11 @@ SELECT * FROM X;
 INTERSECT
 SELECT * FROM Y;
 --X JOIN Z
+SELECT * FROM X;
+INNER JOIN Y;
 --X UNION Y
+SELECT * FROM X;
+UNION Y;
 --Y PRODUCTO Z
 --SELECCION (A - C) de X PRODUCTO Y
 --PROYECCION de (X.C,Y.A) de (SELECCION ((X.B)-(Y.B))
